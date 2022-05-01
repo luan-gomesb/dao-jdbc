b21673ae82dd7d216e28d3e12ff58fb2696e8eb7
luan-gomesb <luan.gomesb@live.com>
2022-04-30 21:28:11 -0300

    add serial annotation on department entity

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,11 +1,29 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department dep = new Department(1,"Teste");
-        System.out.println(dep.toString());
+        SimpleDateFormat dmy = new SimpleDateFormat("dd/MM/yyy");
+        Date birthDate = null;
+        Department dep = new Department(1,"Computers");
+        try{
+            birthDate = dmy.parse("26/03/1994");
+            Seller seller = new Seller(1,
+                    "Luan Gomes",
+                    "luan.gomesb@live.com",
+                    birthDate,
+                    3000.00,
+                    dep);
+            System.out.println(seller);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
--- src/main/java/model/entities/Department.java
+++ src/main/java/model/entities/Department.java
@@ -1,8 +1,10 @@
 package model.entities;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 public class Department implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
     private Integer id;
     private String name;
