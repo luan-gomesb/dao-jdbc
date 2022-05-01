bd513e7424403f76d46f19a1d8c51f18e1ca13ef
luan-gomesb <luan.gomesb@live.com>
2022-05-01 00:59:47 -0300

    test and show find sellers by department

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,19 +1,32 @@
 package application;
 
 import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class Program {
     public static void main(String[] args) {
-        //get seller by id test
-        Integer id = 6;
-        Seller seller =  DaoFactory.createSellerDao().findById(id);
-        System.out.println(seller);
-   }
-}
+        // get seller by id test
+        Integer id = 1;
+        Department dep = new Department();
+        dep.setId(id);
+        dep.setName("Computers");
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        List<Seller> depSellers = sellerDao.findByDepartment(dep);
+
+        Consumer<Seller> showSeller = seller -> System.out.printf("%d - %s - %.2f\n",
+                seller.getId(),seller.getName(),seller.getBaseSalary());
+
+        System.out.print("--------- ");
+        System.out.printf("%s Department Sellers ", dep.getName());
+        System.out.print("---------\n");
+
+        depSellers.stream().forEach(showSeller);
+    }
+
+}
