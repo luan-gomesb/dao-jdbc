df42a35b04e36579540ee899953d5fe6a09ba36b
luan-gomesb <luan.gomesb@live.com>
2022-04-30 23:00:50 -0300

    add DBConnection on dao factory for sellert JDBC

--- src/main/java/model/dao/DaoFactory.java
+++ src/main/java/model/dao/DaoFactory.java
@@ -1,11 +1,16 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
+import java.sql.Connection;
+
 public class DaoFactory {
+
     public static SellerDao createSellerDao(){
-        return new SellerDaoJDBC();
+        Connection conn =  DB.getConnecttion();
+        return new SellerDaoJDBC(conn);
     }
     public static DepartmentDao createDepartmentDao(){
         return new DepartmentDaoJDBC();
